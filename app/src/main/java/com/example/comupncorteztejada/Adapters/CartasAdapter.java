3c8503e362dd09f1ab9f6d8a403e1548117e5ded
VictorCT20 <N00218232@upn.pe>
2023-07-06 09:33:22 -0500

    CuartoPush

--- app/src/main/java/com/example/comupncorteztejada/Adapters/CartasAdapter.java
+++ app/src/main/java/com/example/comupncorteztejada/Adapters/CartasAdapter.java
@@ -1,15 +1,22 @@
 package com.example.comupncorteztejada.Adapters;
 
 import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.comupncorteztejada.DetalleCartaActivity;
+import com.example.comupncorteztejada.DetalleDuelistaActivity;
 import com.example.comupncorteztejada.Entities.Cartas;
 import com.example.comupncorteztejada.R;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -29,7 +36,7 @@ public class CartasAdapter extends RecyclerView.Adapter {
         CartasAdapter.NameViewHolder viewHolder;
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
         if(viewType == 1) {
-            View view = inflater.inflate(R.layout.item_string_coment, parent, false);
+            View view = inflater.inflate(R.layout.item_carta, parent, false);
             viewHolder = new CartasAdapter.NameViewHolder(view);
         } else {
             View view = inflater.inflate(R.layout.item_progressbar, parent, false);
@@ -40,6 +47,27 @@ public class CartasAdapter extends RecyclerView.Adapter {
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        Cartas carta = cartas.get(position);
+
+        if(carta == null) return;
+
+        View view = holder.itemView;
+
+        TextView tvNombre = view.findViewById(R.id.tvNombreCarta);
+        ImageView img = view.findViewById(R.id.ivCarta);
+        Picasso.get().load(carta.getImagen())
+                .resize(300, 400) //tamaño específico
+                .into(img);
+        tvNombre.setText(carta.getNombre());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =  new Intent(context, DetalleCartaActivity.class);
+                intent.putExtra("position", carta.getId());
+                context.startActivity(intent);
+            }
+        });
 
     }
 
@@ -60,7 +88,7 @@ public class CartasAdapter extends RecyclerView.Adapter {
         }
     }
 
-    public void setMovimiento(List<Movimientos> movimientos) {
-        this.items = movimientos;
+    public void setCartas(List<Cartas> carta) {
+        this.cartas = carta;
     }
 }
---
+++ app/src/main/java/com/example/comupncorteztejada/ListaDuelistaActivity.java
@@ -0,0 +1,14 @@
+package com.example.comupncorteztejada;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListaDuelistaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_duelista);
+    }
+}
